421dd38f1c7e69a82c138c72659fcdde192e3312
Ramin Ahmadi <raminahmadi@live.com>
2018-11-11 21:33:43 +1100

    inline edit repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { InlineEditComponent } from './components/inline-edit-component/inline-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InlineEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
